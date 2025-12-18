b7428bf556059b8a8fa792736cdc7081f2fd7aa7
rayaesthe <31704167+rayaesthe@users.noreply.github.com>
2025-12-18 20:10:09 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-  export default defineConfig({
+  export default defineConfig({ base: '/Smartbuildingmanagementappcommunity/' 
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
